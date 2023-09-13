15742d6c769ae11c26837c3575da56974086ad06
Simranmehta-dev <116286250+Simranmehta-dev@users.noreply.github.com>
2023-09-13 22:54:51 +0530

    Time: 2 ms (75.35%), Space: 6.8 MB (9.19%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -7,7 +7,7 @@ public:
 
         return Recursion(n-1)+Recursion(n-2);
     }
-    int Tabulation(int n,vector<int> &dp){
+    int Memoization(int n,vector<int> &dp){
         if(n<=1){
             return n;
         }
@@ -15,14 +15,28 @@ public:
             return dp[n];
         }
 
-        dp[n]=Tabulation(n-1,dp)+Tabulation(n-2,dp);
+        dp[n]=Memoization(n-1,dp)+Memoization(n-2,dp);
 
+        return dp[n];
+    }
+    
+    int Tabulation(int n){
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        dp[1]=1;
+        int s=0;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
     int fib(int n) {
         //return Recursion(n);
 
-        vector<int>dp(n+1,-1);
-        return Tabulation(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return Memoization(n,dp);
+        
+        if(n==0||n==1)return n;
+        return Tabulation(n);
     }
 };
